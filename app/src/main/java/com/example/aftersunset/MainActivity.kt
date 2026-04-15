a7be97ce6482a448c889acd8c84aafef01276c87
ecastellano <ecastellano@dbrautomation.com>
2026-04-15 15:06:17 +0200

    [Splash]: Realizada una pantalla animada de inicio

--- app/src/main/java/com/example/aftersunset/MainActivity.kt
+++ app/src/main/java/com/example/aftersunset/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.aftersunset
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.aftersunset.navigation.Navigation
 import com.example.aftersunset.ui.theme.AfterSunsetTheme
