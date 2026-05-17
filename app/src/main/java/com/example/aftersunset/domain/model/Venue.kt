9f36e6e2ace8a0cd3bb7e52b728018641283a8bb
Jose <josegomezzmlg@gmail.com>
2026-05-17 13:03:00 +0200

    Agregar informacion clubes y reseñas

--- app/src/main/java/com/example/aftersunset/domain/model/Venue.kt
+++ app/src/main/java/com/example/aftersunset/domain/model/Venue.kt
@@ -1,31 +1,46 @@
 package com.example.aftersunset.domain.model
 
-/**
- * Representa un local o establecimiento de ocio nocturno.
- * Contiene información física, técnica y descriptiva de la sala.
- *
- * @property id Identificador único del local.
- * @property name Nombre comercial de la sala.
- * @property zone Área geográfica donde se ubica .
- * @property address Dirección postal completa.
- * @property latitude Latitud para geolocalización.
- * @property longitude Longitud para geolocalización.
- * @property capacity Capacidad máxima de personas permitida.
- * @property minAge Edad mínima requerida para el acceso al local.
- * @property mainPhoto URL de la imagen representativa del establecimiento.
- * @property description Reseña detallada sobre el estilo y ambiente del local.
- */
 
+
+import com.google.firebase.firestore.PropertyName
 
 data class Venue(
-    val id: String="",
-    val name: String="",
-    val zone: String="",
-    val address: String="",
-    val latitude: Double=0.0,
-    val longitude: Double=0.0,
-    val capacity: Int=0,
-    val minAge: Int=18,
-    val mainPhoto: String="",
-    val description: String=""
+
+    val id: String = "",
+
+    @get:PropertyName("nombre")
+    @set:PropertyName("nombre")
+    var name: String = "",
+
+    @get:PropertyName("tipo_local")
+    @set:PropertyName("tipo_local")
+    var zone: String = "",
+
+    @get:PropertyName("direccion")
+    @set:PropertyName("direccion")
+    var address: String = "",
+
+    @get:PropertyName("latitud")
+    @set:PropertyName("latitud")
+    var latitude: Double = 0.0,
+
+    @get:PropertyName("longitud")
+    @set:PropertyName("longitud")
+    var longitude: Double = 0.0,
+
+    @get:PropertyName("aforo")
+    @set:PropertyName("aforo")
+    var capacity: Int = 0,
+
+    @get:PropertyName("edad_minima")
+    @set:PropertyName("edad_minima")
+    var minAge: Int = 18,
+
+    @get:PropertyName("foto_principal")
+    @set:PropertyName("foto_principal")
+    var mainPhoto: String = "",
+
+    @get:PropertyName("descripcion")
+    @set:PropertyName("descripcion")
+    var description: String = ""
 )
